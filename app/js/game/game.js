ab64cefc137d7856d1813c875f1db8cbd5623c29
DimaGashko <dgashko@mail.ua>
2018-04-07 14:33:58 +0300

    render

--- app/js/game/game.js
+++ app/js/game/game.js
@@ -3,7 +3,7 @@
 
    class Game {
       constructor() {
-         _this.init();
+         this._init();
 
          this.render.start();
       }
@@ -14,6 +14,9 @@
                return this.getRenderConfig();
             }
          });
+
+         global.img = new Image();
+         global.img.src = 'favicon.png';
       }
 
       getRenderConfig() {
@@ -24,11 +27,12 @@
             },
             objects: [
                {x: 1, y: 1, w: 1, h: 1},
-               {x: 3, y: 3, w: 25, h: 2},
-               {x: 3, y: -3, w: 1, h: 1},
-               {x: -5, y: 10, w: 1, h: 3},
-               {x: -10, y: 3, w: 3, h: 2},
-               {x: 20, y: 5, w: 5, h: 1}
+               {x: 3, y: 10, w: 2, h: 2},
+               {x: 3, y: 3, w: 1, h: 1},
+               {x: 5, y: 15, w: 1, h: 3},
+               {x: 10, y: 3, w: 3, h: 2},
+               {x: 20, y: 5, w: 5, h: 1},
+               {x: 50, y: 20, w: 10, h: 10, img: global.img}
             ],
          }
       }
--- app/js/game/render.js
+++ app/js/game/render.js
@@ -4,7 +4,12 @@
    var startCount = 1;
 
    var DEF = {
-      getRenderConfig: () => {}
+      scaleX: 10,
+      scaleY: 10, 
+
+      getRenderConfig: () => {
+         return {};
+      }
    }
 
    class Render {
@@ -49,11 +54,7 @@
          startCount++;
       }
 
-      tik() {
-         this.clear();
-         this.render(this.options.getRenderConfig())
-      }
-
+      
       resize() {
          this.updateMetrics();
 
@@ -70,6 +71,47 @@
 
       clear() {
          this.ctx.clearRect(0, 0, this.metrics.gameW, this.metrics.gameH);
+      }
+
+      tik() {
+         this.clear();
+         this._render()
+      }
+
+      _render() {
+         var config = this.options.getRenderConfig();
+         var objects = config.objects;
+         var cameraX = config.cameraX;
+         var cameraY = config.cameraY;
+
+         var ctx = this.ctx;
+         var scaleX = this.options.scaleX;
+         var scaleY = this.options.scaleY;
+
+         for (var i = objects.length - 1; i >= 0; i--) {
+            var obj = objects[i];
+
+            var x = obj.x * scaleX;
+            var y = obj.y * scaleY;
+            var w = obj.w * scaleX;
+            var h = obj.h * scaleY;
+
+            if (!this._isVisible(x, y, w, h, cameraX, cameraY)) {
+               continue; 
+            }
+
+            if (obj.img) {
+               ctx.drawImage(obj.img, x, y, w, h);
+            } else {
+               ctx.fillRect(x, y, w, h);
+            }
+           
+         }
+         
+      }
+
+      _isVisible(x, y, w, h, cameraX, cameraY) {
+         return true;
       }
 
       _getCtx() {
