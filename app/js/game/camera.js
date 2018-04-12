3178eaf45fd51dc3bee9dc72525371462737f4b7
DimaGashko <dgashko@mail.ua>
2018-04-12 22:32:07 +0300

    edit

--- app/js/game/camera.js
+++ app/js/game/camera.js
@@ -59,23 +59,70 @@
          camera.coords = player.getCenter();
       },
 
-      smart: function (camera, player, gameSize) {
+      smart: function smart (camera, player, gameSize) {
          var offset = gameSize.scale(camera.options.offsetScale).scalarAbs();
-      
+         var time = Date.now();
+         var backSpeed = 0.02;
+         var wait = 2000;
+         
          //Horizontal;
          if (player.right > camera.coords.x + offset.x) {
             camera.coords.x = player.right - offset.x;
+            smart.changeX = time;
+            smart.coordsX = camera.coords.x;
 
-         }  if (camera.coords.x > player.coords.x + offset.x) {
+         } else if (camera.coords.x > player.coords.x + offset.x) {
             camera.coords.x = player.left + offset.x;
+            smart.changeX = time;
+            smart.coordsX = camera.coords.x;
+
+         } else if(time - smart.changeX > wait) {
+            console.log(camera.coords.x, smart.coordsX)
+            if (player.coords.x > camera.coords.x) {
+               camera.coords.x += backSpeed;
+
+               if (camera.coords.x > player.coords.x) {
+                  camera.coords.x = player.coords.x;
+               }
+
+            } else if (camera.coords.x > player.coords.x) {
+               camera.coords.x -= backSpeed;
+
+               if (player.coords.x > camera.coords.x) {
+                  camera.coords.x = player.coords.x;
+               }
+
+            }
          }
 
          //Vectical
          if (player.bottom > camera.coords.y + offset.y) {
             camera.coords.y = player.bottom - offset.y;
+            smart.changeY = time;
+            smart.coordsY = camera.coords.y;
 
-         } if (camera.coords.y > player.coords.y + offset.y) {
+         } else if (camera.coords.y > player.coords.y + offset.y) {
             camera.coords.y = player.top + offset.y;
+            smart.changeY = time;
+            smart.coordsY = camera.coords.y;
+         
+         } else if(camera.coords.y != smart.coordsY && time - smart.changeY > wait) {
+
+            if (player.coords.y > camera.coords.y) {
+               camera.coords.y += backSpeed;
+
+               if (camera.coords.y > player.coords.y) {
+                  camera.coords.y = player.coords.y;
+               }
+
+            } else if (camera.coords.y > player.coords.y) {
+               camera.coords.y -= backSpeed;
+
+               if (player.coords.y > camera.coords.y) {
+                  camera.coords.y = player.coords.y;
+               }
+
+            }
          }
 
       },
