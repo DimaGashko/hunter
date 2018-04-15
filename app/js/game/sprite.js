6d0c7c6dbafe16af6a9423e6db408713f48cc636
DimaGashko <dgashko@mail.ua>
2018-04-15 14:11:21 +0300

    editSprite

--- app/js/game/sprite.js
+++ app/js/game/sprite.js
@@ -10,6 +10,8 @@
    var DEF = {
       tileset: 'tileset.png', //Путь к тайлсету со спрайтами
 
+      size: new Vector(32, 32), //Размеры элемента канваса/спрайта
+
       //Объект, сореджащий кадры анимаций по типам
       //Тип передается при вызове метода start
       //И испльзуется для выбора кадров, которые стоит отрисовывать
@@ -30,19 +32,64 @@
       }
    }
 
-
    class Sprite {
       constructor(options = {}) {
          this._createParametrs(options);
          this._init();
       }
 
+      _init() {
+         this._loadTileset().then(() => {
+            this._createCanvas();
+            this._setSize();
+
+         }, () => {
+            console.error(
+               'Не удалось загрузить tileset',
+               this.options.tileset
+            );
+         });
+      }
+
+      /**
+       * Начинает анимацию спрайта переданного типа
+       * 
+       * @param {string} type типа анимации
+       * Праметры типа - options.cadrs[type]
+       * 
+       * Пока не загружен tileste ничего не делает.
+       */
       start(type) {
+         if (this.tileset === null) return;
+
 
       }
 
-      _init() {
+      /**
+       * Меняет размеры канваса/спрайта
+       * 
+       * @param {Vector} newSize новый размер
+       * 
+       * Все что нарисовано на спрайте будет стерто
+       */
+      changeSize(newSize) {
+         this.options.size = newSize;
+         this._setSize();
+      }
 
+      _loadTileset() {
+         return new Promise((resolve, reject) => {
+            
+         });
+      }
+
+      _clear() {
+         this.ctx.clearRect(0, 0, this.options.w, this.options.h);
+      }
+
+      _setSize() {
+         this.canv.width = this.options.w;
+         this.canv.height = this.options.h;
       }
 
       _createCanvas() {
@@ -54,7 +101,9 @@
          this.options = extend(true, {}, DEF, options);
          this.timer = 0; //Таймер, используемый для setTimeout
 
-
+         this.tileset = null;
+         this.canvas = null;
+         this.ctx = null;
       }
    }
 
@@ -126,15 +175,6 @@
 
             setTimeout(tik, o.animation[o.cur].duration);
          }, o.animation[o.cur].duration);
-      }
-
-      _clear() {
-         this.ctx.clearRect(0, 0, this.options.w, this.options.h);
-      }
-
-      _setSize() {
-         this.canv.width = this.options.w;
-         this.canv.height = this.options.h;
       }
 
       _draw() {
