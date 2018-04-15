7bdd160060a7cbd15c5164fa11d57c264042c9c2
DimaGashko <dgashko@mail.ua>
2018-04-15 14:24:44 +0300

    loadTileset

--- app/js/game/sprite.js
+++ app/js/game/sprite.js
@@ -5,7 +5,7 @@
    //Содержит поля у который ключ - это путь к картинке
    //А значение - либо DOM элемент tileset-a (Image)
    //Либо null если картинку с таким адресом загрузить не удалось
-   var tilesets = {} 
+   var loadedTilests = {} 
 
    var DEF = {
       tileset: 'tileset.png', //Путь к тайлсету со спрайтами
@@ -77,9 +77,54 @@
          this._setSize();
       }
 
+      /**
+       * Загружает tileset по адресу из options.tileset
+       * 
+       * Возвращает промис
+       * 
+       * Кеширует tileset-ы, которые удалось и не удалось загрузить 
+       */
       _loadTileset() {
+         var src = this.options.tileset;
+
          return new Promise((resolve, reject) => {
+            //Проверка закешированных tileset-ов
+            var loaded = loadedTilests[src];
+
+            //Закешированно, но не было загруженно
+            if (loaded === null) {
+               
+               reject();
+
+               return;
             
+            //Закешированно и удачно загруженно
+            } else if (loaded instanceof Image) {
+               this.tileset = loaded;
+
+               resolve();
+
+               return;
+            }
+               
+            var tileset = new Image();
+
+            tileset.onload = () => {
+               loadedTilests[src] = tileset; //кешируем tilest
+
+               this.tileset = tileset;
+
+               resolve();
+            }
+
+            tileset.onerror = () => {
+               loadedTilests[src] = null; //кешируем, что такого tileset-a нет
+               
+               reject();
+            }
+
+            tileset.src = src;
+
          });
       }
 
