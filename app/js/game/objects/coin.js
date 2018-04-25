057b51f9441ce1fc4142a6122538c65b284b009e
DimaGashko <dgashko@mail.ua>
2018-04-25 13:47:11 +0300

    edit

--- app/js/game/objects/coin.js
+++ app/js/game/objects/coin.js
@@ -1,18 +1,53 @@
 ;(function(global){
    "use strict"
 
+   var DEF = {
+      springSpeed: 0.08,
+      springDist: 0.1,
+   }
+
    class Coin extends Game.Block {
       constructor(options, collisions) {
+         options = extend(true, {}, DEF, options);
          super(options, collisions);
       }
 
       tik() { 
          super.tik.apply(this, arguments);
-         
+         this._animate();
+
+      }
+
+      _animate() { 
+         this.coords = this._getNextCoords();
+      }
+
+      _getNextCoords() { 
+         this.updateSpring();
+
+         return this._startCoords.plus(this.getSpringVector());
+      }
+
+      getSpringVector() {
+         return new Vector(
+            0,
+            Math.sin(this.spring) * this.options.springDist
+         );
+      }
+
+      updateSpring() {
+         this.spring += this.options.springSpeed;
       }
 
       respondInteraction(obj, side) {
-         
+         this.size = new Vector(0, 0);
+      }
+
+      _createParametrs() {
+         super._createParametrs.apply(this, arguments);
+
+         this._startCoords = this.coords.copy();
+         this.spring = Math.random() * Math.PI * 2;
       }
 
    }
