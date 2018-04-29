bfc4d4ac013a29676cea603a417bc699d0be8353
DimaGashko <dgashko@mail.ua>
2018-04-29 14:33:14 +0300

    healthAnimate

--- app/js/game/health.js
+++ app/js/game/health.js
@@ -1,6 +1,9 @@
 ;(function(global){
    'use strict'
    
+   const HEARTS_COUNT = 10 //количество сердечек-жизней
+   const ONE_HEART = 100 / HEARTS_COUNT; //проент жизний одного сердечка
+
    var DEF = {
       
    }
@@ -30,14 +33,50 @@
          this.actor = actor;
 
          this._initActorEvents();
-         this.update();
+         this.update('noAnimate');
       }
 
       /**
        * Обновляет значение жизней actor-а
        */
-      update() {
-         this.root.innerHTML = this.getHealthPercent() + '%';
+      update(noAnimate) {
+         this.els.prev.innerHTML = this.els.real.innerHTML
+         this.els.real.innerHTML = this._getHTML();
+
+         if (!noAnimate) this.startBlink();
+      }
+
+      startBlink() { 
+         var blinkCount = 0;
+
+         if (this.blinkCount !== 0) { 
+            clearTimeout(this.blinkTimer);
+            this.blinkTimer = 0;
+         }
+
+         this.els.real.classList.add('health__real-blink');
+
+         this.blinkTimer = setTimeout(() => { 
+            this.els.real.classList.remove('health__real-blink');
+         }, 1000);
+      }
+
+      _getHTML() { 
+         var hearts = this.getHealthPercent() / ONE_HEART;
+         
+         var full = hearts ^ 0;
+         var half = (hearts - (hearts ^ 0) > 0) ? 1 : 0;
+         var empty = HEARTS_COUNT - full - half;
+         console.log(full, half, empty)
+         var html = this._getHeartHtml('full', full);
+
+         if (half) { 
+            html += this._getHeartHtml('half', 1);
+         }
+
+         html += this._getHeartHtml('empty', empty);
+         //console.log(html)
+         return html;
       }
 
       getHealthPercent() { 
@@ -49,7 +88,9 @@
       }  
 
       _getHTMLElements() { 
-         var r = this.root = document.querySelector('.health');
+         var r = this.els.root = document.querySelector('.health');
+         this.els.real = r.querySelector('.health__real');
+         this.els.prev = r.querySelector('.health__prev');
       }
    
       //Иницилизирует события
@@ -58,7 +99,11 @@
             this.update();
          })
       }
-   
+      
+      _getHeartHtml(type = 'full', count = 1) { 
+         var one = `<div class="health__heart health__heart-${type}"></div>`;
+         return new Array(count + 1).join(one);
+      }
 
       //Создает основные параметры испльзуемые конструктором
       //options - передаются из конструктора при иницилизации
@@ -68,6 +113,8 @@
          this.els = {};
 
          this.actor = null;
+
+         this.blinkTimer = 0;
 
       }
    
